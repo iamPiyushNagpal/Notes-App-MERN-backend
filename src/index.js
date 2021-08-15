ebfd6243cc18ceca129f2b7cc37e8d3db163d7c8
iamPiyushNagpal <iampiyushnagpal@gmail.com>
2021-08-15 17:37:48 +0530

    Enable cors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ require('./database/db');
 const port = process.env.PORT || 3001;
 const userRoute = require('./routes/userRoute');
 const noteRoute = require('./routes/noteRoute');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use(userRoute);
 app.use(noteRoute);
