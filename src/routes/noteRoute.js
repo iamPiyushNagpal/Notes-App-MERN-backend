f47642b0799c5f5b61419a7c2be3e6169fe39700
iamPiyushNagpal <iampiyushnagpal@gmail.com>
2021-08-26 10:07:23 +0530

    Protect update and delete note routes

--- src/routes/noteRoute.js
+++ src/routes/noteRoute.js
@@ -5,12 +5,12 @@ const passport = require('passport');
 
 router.post('/create-note', passport.authenticate('jwt', { session: false }), noteController.createNote);
 
-router.delete('/delete-note', noteController.deleteNote);
+router.delete('/delete-note', passport.authenticate('jwt', { session: false }), noteController.deleteNote);
 
 router.get('/get-notes', passport.authenticate('jwt', { session: false }), noteController.getNotes);
 
-router.get('/get-note', noteController.getNote);
+router.get('/get-note', passport.authenticate('jwt', { session: false }), noteController.getNote);
 
-router.patch('/update-note', noteController.updateNote);
+router.patch('/update-note', passport.authenticate('jwt', { session: false }), noteController.updateNote);
 
 module.exports = router;
